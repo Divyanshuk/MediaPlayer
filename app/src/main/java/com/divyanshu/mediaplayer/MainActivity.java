f999330a36b448720cc1e577d34861422db5d03d
divyanshukumar <divyanshuk34@gmail>
2018-09-02 23:32:12 +0530

    added a song and play and pause functionality

--- app/src/main/java/com/divyanshu/mediaplayer/MainActivity.java
+++ app/src/main/java/com/divyanshu/mediaplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.divyanshu.mediaplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button play = findViewById(R.id.play);
+        Button pause = findViewById(R.id.pause);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.sample);
+
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mediaPlayer.start(); // no need to call prepare(); create() does that for you
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                } else {
+                    mediaPlayer.start();
+                }
+            }
+        });
     }
 }
