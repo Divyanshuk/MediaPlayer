7d350fc7b88bb77da4ebb858c29b94a104e45859
divyanshukumar <divyanshuk34@gmail>
2018-09-03 00:27:46 +0530

    some memory management by using activity life cycle

--- app/src/main/java/com/divyanshu/mediaplayer/MainActivity.java
+++ app/src/main/java/com/divyanshu/mediaplayer/MainActivity.java
@@ -3,20 +3,25 @@ package com.divyanshu.mediaplayer;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+     MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v("MainActivity", "onCreate");
         setContentView(R.layout.activity_main);
 
         Button play = findViewById(R.id.play);
         Button pause = findViewById(R.id.pause);
 
-        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.sample);
+        mediaPlayer = MediaPlayer.create(this, R.raw.sample);
+
 
 
         play.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +42,45 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.v("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("MainActivity", "onResume");
+
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        Log.v("MainActivity", "onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("MainActivity", "onStop");
+
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("MainActivity", "onDestroy");
+
     }
 }
